ed084630174ab026c008b5f0898c24d69f5c681d
Rodolfo Guzman <rodolfoguzman147@gmail.com>
2018-08-03 01:55:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog
 
-def instantiate(name,breeds)
-  @breeds=breeds
-  @name=name
-end
+  def instantiate(name,breeds)
+    @breeds=breeds
+    @name=name
+  end
 
-def name
-  @name
-end
+  def name
+    @name
+  end
 
-def breeds
-  @breeds
-end
+  def breeds
+    @breeds
+  end
 
-def name=(new_name)
-  @name=new_name
-  @breeds=
-end
+  def name=(new_name)
+    @name=new_name
+    @breeds=
+  end
 
 end
 
